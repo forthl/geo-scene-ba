fcbce8d98517009192ae8a543d0e995ac26a173d
Lukas Schneidt <lukas@algoriddim.com>
2023-06-30 16:53:42 +0200

    added visualization of clustering performance

--- maskDepth.py
+++ maskDepth.py
@@ -7,12 +7,6 @@ from kneed import KneeLocator
 from scipy.spatial.distance import cdist
 from sklearn.mixture import GaussianMixture
 
-def normalize_array(arr):
-    min_val = np.min(arr)
-    max_val = np.max(arr)
-    normalized_arr = (arr - min_val) / (max_val - min_val) * 255
-    return normalized_arr
-
 # get masks from segmentations
 def get_segmentation_masks(img_path):
     masks = []
@@ -27,16 +21,13 @@ def get_segmentation_masks(img_path):
 # mask depth image with segmentations
 def get_masked_depth(depth_path, masks):
     D = Image.open(depth_path)
-    depth_array = np.asarray(D)
-    depth_array = normalize_array(depth_array)
+    depth_array = (np.asarray(D) -1) / 256
+
     masked_depths = []
     for mask in masks:
         seg_masked = np.where(mask, depth_array, 0)
         masked_depth = np.uint8(seg_masked)
-        masked_depth = normalize_array(masked_depth)
         masked_depths.append(masked_depth)
-        # masked_depth = Image.fromarray(masked_depth)
-        # masked_depth.show()
     return masked_depths
 
 
@@ -48,11 +39,20 @@ def save_masks(masked_depths):
 
 
 def create_point_clouds(masked_depths):
+    # parameters taken from aachen_000000_000019_camera.json
+    fx = 2262.52
+    fy = 2265.3017905988554
+    cx = 1096.98
+    cy = 513.137
+    b = 0.209313
+
     point_clouds = []
     for mask in masked_depths:
         non_zero = np.nonzero(mask)
+        depth = b * fx / (mask[non_zero[0], non_zero[1]])
         point_cloud = np.array(
             [non_zero[0], non_zero[1], mask[non_zero[0], non_zero[1]]])
+        #point_cloud = np.array([(non_zero[0] - cx) * depth / fx, (non_zero[1] - cy) * depth / fy, b * fx / depth])
         point_clouds.append(point_cloud)
     return point_clouds
 
@@ -67,13 +67,23 @@ def optimal_k_elbow(data, max_k):
     return kn.knee
 
 def optimal_k_bic(data, max_k):
+    ks = np.arange(1, max_k)
     bics = []
-    for k in range(1, max_k):
+    for k in ks:
         gmm = GaussianMixture(n_components=k, init_params='kmeans')
         gmm.fit(data)
         bics.append(gmm.bic(data))
 
-    return np.argmin(bics)
+    # Plot the data
+    fig, ax = plt.subplots()
+    ax.plot(ks, bics)
+    ax.set_xlabel(r'Number of clusters, $k$')
+    ax.set_ylabel('BIC')
+    ax.set_xticks(ks);
+
+    diff = [x - bics[i - 1] for i, x in enumerate(bics)][1:]
+
+    return diff.index(min(diff)) + 2
 
 def optimal_k_ml(data, max_k):
     #TODO
@@ -122,7 +132,7 @@ def visualize_clusters(data, labels, centroids):
 
     plt.show()
 
-def find_clusters(point_cloud, max_k, optimal_k_method):
+def find_clusters_kmeans(point_cloud, max_k, optimal_k_method):
     data = np.transpose(point_cloud)
     optimal_k = find_optimal_k(data, max_k, optimal_k_method)
     print(f"Optimal k: {optimal_k}")
@@ -135,25 +145,51 @@ def distance(x, y, centroids):
         dist = np.linalg.norm(np.array([x, y]) - np.array([centroid[0], centroid[1]]))
     return 0
 
+def visualize_clusters(masked_depths, sampleIdx):
+
+    sample = masked_depths[sampleIdx] # 1 for cars, 10 for street lamps
+    x = create_point_clouds([sample])
+    x = x[0]
+
+    labels, centroids, data = find_clusters_kmeans(x, 20, optimal_k_method='bic')
+    c = np.transpose(centroids)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.scatter3D(x[0], x[1], x[2], c=labels);
+    ax.scatter3D(c[0], c[1], c[2], 'black');
+    plt.xlim(0, 1024)
+    plt.ylim(0, 2048)
+    ax.set_zlim(0, 255)
+    plt.show()
+
+
 if __name__ == '__main__':
     img_path = "tmp_data/aachen_000000_000019_gtFine_color.png"
     depth_path = "tmp_data/aachen_000000_000019_disparity.png"
 
+    sampleIdx = 1 # 1 for cars, 10 for street lamps, 6 for street
+
     masks = get_segmentation_masks(img_path)
+    Image.fromarray(masks[sampleIdx]).show()
     masked_depths = get_masked_depth(depth_path, masks)
     # save_masks(masked_depths)
+    visualize_clusters(masked_depths, sampleIdx)
     point_clouds = create_point_clouds(masked_depths)
 
 
-    mask = masks[1]
-    point_cloud = point_clouds[1]
-    labels, centroids, data = find_clusters(point_cloud, 20, optimal_k_method='bic')
+    x = False
+    if x:
+        mask = masks[3]
+        point_cloud = point_clouds[3]
+        labels, centroids, data = find_clusters(point_cloud, 20, optimal_k_method='bic')
 
-    for j in range(len(centroids)):
-        tmp = np.zeros(mask.shape)
-        for i in range(len(data)):
-            if labels[i] == j:
-                tmp[int(data[i][0]), int(data[i][1])] = 255
-        tmp = Image.fromarray(tmp).convert('RGB')
-        #tmp.show()
+        show_images = False
+        if show_images:
+            for j in range(len(centroids)):
+                tmp = np.zeros(mask.shape)
+                for i in range(len(data)):
+                    if labels[i] == j:
+                        tmp[int(data[i][0]), int(data[i][1])] = 255
+                tmp = Image.fromarray(tmp).convert('RGB')
+                tmp.show()
 
